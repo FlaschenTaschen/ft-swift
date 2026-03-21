a7ba87a6fa23ee4ddfc352805fac6a387a6a8aab
Brennan Stehling <277419+brennanMKE@users.noreply.github.com>
2026-03-20 22:33:57 -0700

    fixes SimpleExample

--- Sources/simple-example/SimpleExample.swift
+++ Sources/simple-example/SimpleExample.swift
@@ -7,19 +7,30 @@ nonisolated private let logger = Logger(subsystem: Logging.subsystem, category: 
 
 @main
 struct SimpleExample {
-    static func main() {
-        let args = ArgumentPreprocessor.preprocess(args: CommandLine.arguments)
-        let argString = args.count > 1 ? args.dropFirst().joined(separator: " ") : "(none)"
-        logger.info("Arguments: \(argString, privacy: .public)")
+    static func main() async {
+        let standardOptions = StandardOptions(args: CommandLine.arguments)
 
-        let hostname = args.count > 1 ? args[1] : nil
+        if standardOptions.showUsage {
+            printUsage()
+            exit(EXIT_SUCCESS)
+        }
+
+        let args = standardOptions.nonStandardArgs
+
+        let hostname = args.count > 0 ? args[0] : nil
 
         let socket = openFlaschenTaschenSocket(hostname: hostname)
         let canvas = UDPFlaschenTaschen(fileDescriptor: socket, width: 25, height: 20)
 
         let options = SimpleExampleDemo.Options(hostname: hostname, width: 25, height: 20)
-        Task {
-            await SimpleExampleDemo.run(options: options, canvas: canvas)
-        }
+        await SimpleExampleDemo.run(options: options, canvas: canvas)
+    }
+
+    static func printUsage() {
+        print("Simple Example (c) 2016 Carl Gorringe (carl.gorringe.org)")
+        print("Usage: simple-example [options]")
+        print("Options:")
+        StandardOptions.printStandardOptions()
+        print("  <host>    : FT host")
     }
 }
